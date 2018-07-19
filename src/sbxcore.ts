5521dfb39b34ae9e060224278e314d4525249255
Alexander Taborda Acosta <alextab93@gmail.com>
2018-07-19 20:46:08 +0000

    add Find and upsert implementation to Core.

--- src/sbxcore.ts
+++ src/sbxcore.ts
@@ -2,111 +2,181 @@ import QueryBuilder from 'sbx-querybuilder/index';
 
 export class SbxCore{
 
-    constructor(){
+  constructor(){ }
+  
+  public static environment = { } as any;
+  public urls: any = {
+    update_password: '/user/v1/password',
+    login: '/user/v1/login',
+    register: '/user/v1/register',
+    validate: '/user/v1/validate',
+    row: '/data/v1/row',
+    find: '/data/v1/row/find',
+    update: '/data/v1/row/update',
+    delete: '/data/v1/row/delete',
+    downloadFile: '/content/v1/download',
+    uploadFile: '/content/v1/upload',
+    addFolder: '/content/v1/folder',
+    folderList: '/content/v1/folder',
+    send_mail: '/email/v1/send',
+    payment_customer: '/payment/v1/customer',
+    payment_card: '/payment/v1/card',
+    payment_token: '/payment/v1/token',
+    password: '/user/v1/password/request',
+    cloudscript_run: '/cloudscript/v1/run'
+  };
 
+  public initialize(domain: number, appKey: string, baseUrl: string = 'https://sbxcloud.com/api') {
+    SbxCore.environment.domain = domain;
+    SbxCore.environment.baseUrl = baseUrl;
+    SbxCore.environment.appKey = appKey;
+  }
+
+  protected with(model: string) {
+    return new Find(model, SbxCore.environment.domain);
+  }
+  
+  protected upsert(model: string, data: any, letNull?: Boolean) {
+    return this.queryBuilderToInsert(data, letNull).setModel(model).compile();
+  }
+  
+  protected encodeEmails(email: string) {
+    const spl = email.split('@');
+    if (spl.length > 1) {
+        email = encodeURIComponent(spl[0]) + '@' + spl[1];
     }
-    public static environment = { } as any;
-    public urls: any = {
-        update_password: '/user/v1/password',
-        login: '/user/v1/login',
-        register: '/user/v1/register',
-        validate: '/user/v1/validate',
-        row: '/data/v1/row',
-        find: '/data/v1/row/find',
-        update: '/data/v1/row/update',
-        delete: '/data/v1/row/delete',
-        downloadFile: '/content/v1/download',
-        uploadFile: '/content/v1/upload',
-        addFolder: '/content/v1/folder',
-        folderList: '/content/v1/folder',
-        send_mail: '/email/v1/send',
-        payment_customer: '/payment/v1/customer',
-        payment_card: '/payment/v1/card',
-        payment_token: '/payment/v1/token',
-        password: '/user/v1/password/request',
-        cloudscript_run: '/cloudscript/v1/run'
-    };
+    return email;
+  }
 
-    public initialize(domain: number, appKey: string, baseUrl: string = 'https://sbxcloud.com/api') {
-        SbxCore.environment.domain = domain;
-        SbxCore.environment.baseUrl = baseUrl;
-        SbxCore.environment.appKey = appKey;
+  protected validateLogin(login: string): boolean {
+    const rlogin =  /^(\w?\.?\-?)+$/;
+    return rlogin.test(login);
+  }
+
+  protected validateEmail(email: string): boolean {
+    const rlogin  =  /^(\w?\.?\-?\+?)+@(\w?\.?\-?)+$/;
+    return rlogin.test(email);
+  }
+
+  /**
+   * UTILS
+   */
+
+  private queryBuilderToInsert(data: any, letNull?: Boolean): any {
+    const query = new QueryBuilder()
+        .setDomain(SbxCore.environment.domain);
+    if (Array.isArray(data) ) {
+      data.forEach(item => {
+        query.addObject(this.validateData(item, letNull));
+      });
+    }else {
+      query.addObject(this.validateData(data, letNull));
     }
+    return query;
+  }
 
+  public validateData(data: any, letNull?: Boolean): any {
+    const temp = {};
+    Object.keys(data).filter(key => {
+      const v = data[key];
+      return (((Array.isArray(v) || typeof v === 'string') ?
+          (v.length > 0) :
+          (v !== null && v !== undefined)) || letNull);
+      }).forEach(key => {
+      if ( (data[key] !== null && data[key] !== undefined)  && data[key]._KEY != null) {
+        data[key] = data[key]._KEY;
+      }
+      const key2 = (key !== '_KEY') ? key.replace(/^_/, '') : key;
+      temp[key2] = data[key];
+    });
+    return temp;
+  }
 
-    protected encodeEmails(email: string) {
-        const spl = email.split('@');
-        if (spl.length > 1) {
-            email = encodeURIComponent(spl[0]) + '@' + spl[1];
-        }
-        return email;
-    }
-
-    protected validateLogin(login: string): boolean {
-        const rlogin   =  /^(\w?\.?\-?)+$/;
-        return rlogin.test(login);
-    }
-
-    protected validateEmail(email: string): boolean {
-        const rlogin  =  /^(\w?\.?\-?\+?)+@(\w?\.?\-?)+$/;
-        return rlogin.test(email);
-    }
-
-    /**
-     * UTILS
-     */
-
-    protected queryBuilderToInsert(data, letNull?: Boolean): any {
-        const query = new QueryBuilder()
-            .setDomain(SbxCore.environment.domain);
-        if (Array.isArray(data) ) {
-            data.forEach(item => {
-                query.addObject(this.validateData(item, letNull));
-            });
+  public validateKeys(data: any): any {
+    const temp = [];
+    if (Array.isArray(data) ) {
+      data.forEach(key => {
+        if (typeof key === 'string') {
+          temp.push(key);
         }else {
-            query.addObject(this.validateData(data, letNull));
+          temp.push(key._KEY);
         }
-        return query;
+      });
+    }else {
+      if (typeof data === 'string') {
+        temp.push(data);
+      }else {
+        temp.push(data._KEY);
+      }
     }
+    return temp;
+  }
+  
+  public mapReverseFetchesResult(response: any, parentName: string, arrayName: string, completefetch?: string[]  ){
+    if(parentName.indexOf(".")>=0) throw Error("Only one level is allowed");
+    completefetch = completefetch || [parentName];
+    const newResponse = this.mapFetchesResult(response, completefetch);
+    const objTemp = newResponse.results.reduce(function( objects, item ) {
+      objects[item[parentName]._KEY] = objects[item[parentName]._KEY] || item[parentName];
+      objects[item[parentName]._KEY][arrayName] = objects[item[parentName]._KEY][arrayName] || [];
+      item[parentName]=item[parentName]._KEY;
+      objects[item[parentName]._KEY][arrayName].push(item);
+    }, {});
+    newResponse.results = Object.keys(objTemp).map( k => objTemp(k));
+    return newResponse;
+  }
+  
+  /**
+   * @param response the response of the server
+   * @param {string[]} completefetch the array of fetch
+   * @returns {any} the response with the union between fetch_results and results
+   */
+  public mapFetchesResult(response: any, completefetch: string[] ) {
+    if (response.fetched_results) {
+      const fetch = [];
+      const secondfetch = {};
+      for (let i = 0; i < completefetch.length; i++) {
+        let index = 0;
+        const temp = completefetch[i].split('.');
+        if (fetch.indexOf(temp[0]) < 0) {
+          fetch.push(temp[0]);
+          index = fetch.length - 1;
+        } else {
+          index = fetch.indexOf(temp[0]);
+        }
+        if (temp.length === 2 && !secondfetch[fetch[index]]) {
+          secondfetch[fetch[index]] = [];
+        }
 
-    public validateData(data: any, letNull?: Boolean): any {
-        const temp = {};
-        Object.keys(data)
-            .filter(key => {
-                const v = data[key];
-                return (((Array.isArray(v) || typeof v === 'string') ?
-                    (v.length > 0) :
-                    (v !== null && v !== undefined)) || letNull);
-            }).forEach(key => {
-            if ( (data[key] !== null && data[key] !== undefined)  && data[key]._KEY != null) {
-                data[key] = data[key]._KEY;
-            }
-            const key2 = (key !== '_KEY') ? key.replace(/^_/, '') : key;
-            temp[key2] = data[key];
-        });
-        return temp;
-    }
-
-    public validateKeys(data: any): any {
-        const temp = [];
-        if (Array.isArray(data) ) {
-            data.forEach(key => {
-                if (typeof key === 'string') {
-                    temp.push(key);
-                }else {
-                    temp.push(key._KEY);
+        if (temp.length === 2) {
+          secondfetch[fetch[index]].push(temp[1]);
+        }
+      }
+      for (let i = 0; i < response.results.length; i++) {
+        for (let j = 0; j < fetch.length; j++) {
+          for (const mod in response.fetched_results) {
+            if (response.fetched_results[mod][response.results[i][fetch[j]]]) {
+              response.results[i][fetch[j]] = response.fetched_results[mod][response.results[i][fetch[j]]];
+              if (secondfetch[fetch[j]]) {
+                for (let k = 0; k < secondfetch[fetch[j]].length; k++) {
+                  const second = secondfetch[fetch[j]][k];
+                  for (const mod2 in response.fetched_results) {
+                    if (response.fetched_results[mod2][response.results[i][fetch[j]][second]]) {
+                      response.results[i][fetch[j]][second] =
+                        response.fetched_results[mod2][response.results[i][fetch[j]][second]];
+                      break;
+                    }
+                  }
                 }
-            });
-        }else {
-            if (typeof data === 'string') {
-                temp.push(data);
-            }else {
-                temp.push(data._KEY);
+              }
+              break;
             }
+          }
         }
-        return temp;
+      }
     }
-
+    return response;
+  }
 }
 
 export class Find {
@@ -116,6 +186,10 @@ export class Find {
   constructor(model: string, domain: number) {
     this.query = new QueryBuilder().setDomain(domain).setModel(model);
     this.lastANDOR = null;
+  }
+  
+  public compile() {
+    return this.query.compile();
   }
 
   public newGroupWithAnd() {
@@ -487,79 +561,6 @@ export class Find {
       }
     }
     return temp;
-  }
-
-
-  public mapReverseFetchesResult(response: any, parentName: string, arrayName: string, completefetch?: string[]  ){
-    if(parentName.indexOf(".")>=0) throw Error("Only one level is allowed");
-      completefetch = completefetch || [parentName];
-      const newResponse = this.mapFetchesResult(response, completefetch);
-      const objTemp = newResponse.results.reduce( function( objects, item ){
-              objects[item[parentName]._KEY] =   objects[item[parentName]._KEY] || item[parentName];
-              objects[item[parentName]._KEY][arrayName] =  objects[item[parentName]._KEY][arrayName] || [];
-              item[parentName]=item[parentName]._KEY;
-              objects[item[parentName]._KEY][arrayName].push(item)
-          }
-          ,{});
-      newResponse.results = Object.keys(objTemp).map( k => objTemp(k));
-      return newResponse;
-
-  }
-  
-  /**
-   * @param response the response of the server
-   * @param {string[]} completefetch the array of fetch
-   * @returns {any} the response with the union between fetch_results and results
-   */
-  public mapFetchesResult(response: any, completefetch: string[] ) {
-
-    if (response.fetched_results) {
-      const fetch = [];
-      const secondfetch = {};
-      for (let i = 0; i < completefetch.length; i++) {
-        let index = 0;
-        const temp = completefetch[i].split('.');
-        if (fetch.indexOf(temp[0]) < 0) {
-          fetch.push(temp[0]);
-          index = fetch.length - 1;
-        } else {
-          index = fetch.indexOf(temp[0]);
-        }
-        if (temp.length === 2 && !secondfetch[fetch[index]]) {
-          secondfetch[fetch[index]] = [];
-        }
-
-        if (temp.length === 2) {
-          secondfetch[fetch[index]].push(temp[1]);
-        }
-      }
-      for (let i = 0; i < response.results.length; i++) {
-        for (let j = 0; j < fetch.length; j++) {
-          for (const mod in response.fetched_results) {
-            if (response.fetched_results[mod][response.results[i][fetch[j]]]) {
-              response.results[i][fetch[j]] = response.fetched_results[mod][response.results[i][fetch[j]]];
-              if (secondfetch[fetch[j]]) {
-                for (let k = 0; k < secondfetch[fetch[j]].length; k++) {
-                  const second = secondfetch[fetch[j]][k];
-                  for (const mod2 in response.fetched_results) {
-                    if (response.fetched_results[mod2][response.results[i][fetch[j]][second]]) {
-                      response.results[i][fetch[j]][second] =
-                        response.fetched_results[mod2][response.results[i][fetch[j]][second]];
-                      break;
-                    }
-                  }
-                }
-              }
-
-              break;
-            }
-          }
-        }
-
-      }
-    }
-
-    return response;
   }
 }
 
